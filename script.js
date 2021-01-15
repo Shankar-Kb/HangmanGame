6ffb02c3ec795371aa2c96a7909a5bbb597ebb55
Shankar-Kb <shankar.kb95@gmail.com>
2021-01-15 23:45:13 +0530

    Added initial version of Hangman Game

---
+++ script.js
@@ -0,0 +1,80 @@
+// Source 1: https://random-word-api.herokuapp.com/
+// Source 2: https://api.wordnik.com/
+
+function createHtmlElement(element,  className='', id=''){
+    var elem = document.createElement(element);
+    elem.setAttribute('class', className);
+    elem.setAttribute('id', id);
+    return elem;
+}
+
+let wordDisplay = document.getElementById('wordBox');
+let randomWord = [];
+async function randomWordGen(){
+    
+    let randomWordResp = await fetch('https://random-word-api.herokuapp.com/word?number=1');
+    let randomWordData = await randomWordResp.json();
+    console.log(randomWordData[0].toUpperCase());
+    randomWord = randomWordData[0].toUpperCase().split('');
+    randomWord.forEach( elem => {
+        var alphabet = createHtmlElement('div', `word ${elem}`)
+        alphabet.innerHTML = `${elem}`;
+        alphabet.style.fontSize = "0px";
+        wordDisplay.append(alphabet);
+    });
+}
+randomWordGen();
+
+
+let keyboardBox = document.getElementById('keysBox');
+let keyboardArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+let countCorrect = 0;
+let countWrong = 0;
+let imageMan = new Image();
+
+keyboardArray.forEach( elem => {
+    var key = createHtmlElement('button', 'btn btn-secondary', `K${elem}`);
+    key.innerHTML = `${elem}`;
+    key.addEventListener('click', function(){
+        pressButton(elem); 
+    });
+    keyboardBox.append(key);
+});
+
+document.body.addEventListener('keydown', function(event){
+   let regExp = /^[A-Za-z]$/;
+   if(regExp.test(event.key)){
+        pressButton(event.key.toUpperCase());
+    }
+});
+
+function pressButton(key){
+    var currentKey = document.getElementById(`K${key}`);
+    currentKey.disabled = true; 
+        if(randomWord.indexOf(key)>=0){
+          var guessedWord = document.getElementsByClassName(`${key}`);
+
+          for (element of guessedWord){
+             element.style.fontSize = "2.5rem";
+             countCorrect++;
+          }
+          if(randomWord.length === countCorrect) displayModal('You Guessed Right!');
+        }
+        else{
+            countWrong++;
+            if(countWrong < 8){
+            imageMan.src = `assets/stage${countWrong}.png`;
+            document.getElementById('pictureBox').append(imageMan);
+            }
+            if(countWrong >= 7) displayModal(`You Lose! The Word was ${randomWord.join('')}`);   
+        }  
+}
+
+function displayModal(string){
+    document.getElementById("modalButton").click();
+    document.getElementById("staticBackdropTitle").innerHTML = string;
+}
+
+function refreshPage(){
+    location.reload();
+}
